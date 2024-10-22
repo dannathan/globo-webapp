3920e6b78162a8517a2680df53af513f9c77b230
dnathan <test@test.com>
2024-10-22 15:42:01 -0400

    Switch to using tfe_outputs

--- datasources.tf
+++ datasources.tf
@@ -5,3 +5,8 @@
 data "aws_ssm_parameter" "amzn2_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+data "tfe_outputs" "networking" {
+  organization = var.tfe_organization   # "deep-dive-globo-dn"
+  workspace    = var.tfe_workspace_name #"web-app-dev"
+}
